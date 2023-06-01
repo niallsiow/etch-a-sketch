601ecad0451d63310803eab596a4a3de33293da6
nisi <49096264+niallsiow@users.noreply.github.com>
2023-06-01 18:13:04 +0100

    Turn grid creation into a function, add placeholder button to create a new grid

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 const grid = document.querySelector('.grid');
 
+const gridPixelWidth = grid.clientWidth;
 
-for(let i = 0; i < 16; i++){
-    for(let j = 0; j < 16; j++){
-        const gridElement = document.createElement('div');
-
-        const list = gridElement.classList;
-        list.add('gridElement');
-
-        gridElement.addEventListener('mouseover', () => gridElement.classList.add('hovered'));
-
-        grid.appendChild(gridElement);
+function makeNewGrid(gridWidth){
+    for(let i = 0; i < gridWidth; i++){
+        for(let j = 0; j < gridWidth; j++){
+            const gridElement = document.createElement('div');
+    
+            const list = gridElement.classList;
+            list.add('gridElement');
+    
+            gridElement.addEventListener('mouseover', () => gridElement.classList.add('hovered'));
+    
+            grid.appendChild(gridElement);
+        }
     }
 }
 
-console.log(grid)
+makeNewGrid(16);
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+    let gridWidth = prompt('Enter Grid Width:', 16);
+    
+    // need to delete old grid first
+
+    makeNewGrid(gridWidth);
+});
