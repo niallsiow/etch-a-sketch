519efce27555a5e09c88023388efa79a6bca1fcc
nisi <49096264+niallsiow@users.noreply.github.com>
2023-06-01 18:22:47 +0100

    Modify program to calculate grid element size based on grid width

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const grid = document.querySelector('.grid');
 const gridPixelWidth = grid.clientWidth;
 
 function makeNewGrid(gridWidth){
+    let gridElementPixelWidth = gridPixelWidth / gridWidth;
+    const borderPixelWidth = 1;
+    gridElementPixelWidth = gridElementPixelWidth - (2 * borderPixelWidth);
+
     for(let i = 0; i < gridWidth; i++){
         for(let j = 0; j < gridWidth; j++){
             const gridElement = document.createElement('div');
-    
-            const list = gridElement.classList;
-            list.add('gridElement');
+
+            gridElement.style.width = gridElementPixelWidth  + 'px';
+            gridElement.style.height = gridElementPixelWidth + 'px';
     
             gridElement.addEventListener('mouseover', () => gridElement.classList.add('hovered'));
     
@@ -17,12 +21,12 @@ function makeNewGrid(gridWidth){
     }
 }
 
-makeNewGrid(16);
+makeNewGrid(64);
 
 const btn = document.querySelector('.btn');
 btn.addEventListener('click', () => {
     let gridWidth = prompt('Enter Grid Width:', 16);
-    
+
     // need to delete old grid first
 
     makeNewGrid(gridWidth);
