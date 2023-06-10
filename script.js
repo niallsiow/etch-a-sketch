f1f2af387fd88a8cc830894bd83a78e255f88189
nisi <49096264+niallsiow@users.noreply.github.com>
2023-06-10 12:15:03 +0100

    Add new rainbow coloring mode

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-const grid = document.querySelector('.grid');
+function getRandomColor(){
+    const num = Math.floor(Math.random() * (0xFFFFFF + 1));
+    return num.toString(16);
+}
 
+const DrawingMode = {
+    Standard: 'standard',
+    Rainbow: 'rainbow'
+}
+
+let currentDrawingMode = DrawingMode.Standard;
+
+function setDrawingMode(mode){
+    currentDrawingMode = mode;
+    makeNewGrid(gridWidth);
+    console.log(currentDrawingMode);
+}
+
+
+const grid = document.querySelector('.grid');
 const gridPixelWidth = grid.clientWidth;
 
 function makeNewGrid(gridWidth){
+    // Get rid of old grid first
+    grid.replaceChildren();
+
     let gridElementPixelWidth = gridPixelWidth / gridWidth;
     const borderPixelWidth = 1;
     gridElementPixelWidth = gridElementPixelWidth - (2 * borderPixelWidth);
@@ -13,15 +34,26 @@ function makeNewGrid(gridWidth){
 
             gridElement.style.width = gridElementPixelWidth  + 'px';
             gridElement.style.height = gridElementPixelWidth + 'px';
-    
-            gridElement.addEventListener('mouseover', () => gridElement.classList.add('hovered'));
+
+            
+            // change this to a switch statement
+            if(currentDrawingMode === DrawingMode.Standard){
+                gridElement.addEventListener('mouseover', () => gridElement.classList.add('hovered'));
+            }
+            else if(currentDrawingMode === DrawingMode.Rainbow){
+                gridElement.addEventListener('mouseover', () => {
+                    console.log('#' + getRandomColor().toUpperCase());
+                    gridElement.style.backgroundColor = '#' + getRandomColor().toUpperCase();
+                });
+            }
     
             grid.appendChild(gridElement);
         }
     }
 }
 
-makeNewGrid(16);
+let gridWidth = 16;
+makeNewGrid(gridWidth);
 
 const btn = document.querySelector('.btn');
 btn.addEventListener('click', () => {
@@ -31,8 +63,11 @@ btn.addEventListener('click', () => {
         gridWidth = prompt('Grid Width entered is too big, please enter a grid width less than 100:', 16);
     }
 
-
-    grid.replaceChildren();
-
     makeNewGrid(gridWidth);
 });
+
+const standardButton = document.querySelector('.standard');
+standardButton.addEventListener('click', () => setDrawingMode(DrawingMode.Standard));
+
+const rainbowButton = document.querySelector('.rainbow');
+rainbowButton.addEventListener('click', () => setDrawingMode(DrawingMode.Rainbow));
